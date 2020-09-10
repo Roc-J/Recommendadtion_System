6642631a988f4b3c1221228f237d3f130986bf2a
Roc-J <646481338@qq.com>
2020-09-10 22:16:53 +0800

    chapter 02

--- Ch02/2-3.py
+++ Ch02/2-3.py
@@ -29,3 +29,16 @@ def Recommendation(train, user_id, W, K):
                 continue
             rank[j] += pi * wj
     return rank
+
+# 带解释的ItemCF算法
+def Recommendation(train, user_id, W, K):
+    rank = dict()
+    ru = train[user_id]
+    for i, pi in ru.items():
+        for j, wj in sorted(W[i].items(), key=itemgetter(1), reverse=True)[0:K]:
+            if j in ru:
+                continue
+            #rank[j] += pi * wj
+            rank[j].weight += pi*wj
+            rank[j].reason[i] = pi* wj
+    return rank
