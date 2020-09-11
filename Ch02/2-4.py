74e399c67f2682864542d815d9fd8f12487fd308
Roc-J <646481338@qq.com>
2020-09-11 19:50:03 +0800

    chapter 02

--- Ch02/2-4.py
+++ Ch02/2-4.py
@@ -37,3 +37,20 @@ def Recommend(user, P, Q):
             if i not in rank:
                 rank[i] += puf * qfi
     return rank
+
+# 基于图的模型
+def PersonalRank(G, alpha, root):
+    rank = dict()
+    rank = { x:0 for x in G.keys()}
+    rank[root] = 1
+    for k in range(20):
+        tmp = {x:0 for x in G.keys()}
+        for i, ri in G.items():
+            for j , wij in ri.items():
+                if j not in tmp:
+                    tmp[j] = 0
+                tmp[j] += 0.6*rank[i]/(1.0*len(ri))
+                if j == root:
+                    tmp[j] += 1-alpha
+        rank = tmp
+    return rank
